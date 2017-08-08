5cd1aed7850e2795ac6fd44d742515dcc3994a67
Thomas <thomasmaher1210@gmail.com>
2017-08-08 04:30:53 -0400

    Enables toggle view for mixing links in sidebar

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -3,13 +3,33 @@ import styles from './SideBar.css';
 import { Link } from 'react-router-dom';
 
 export default class SideBar extends Component {
+  constructor(props) {
+    super(props)
+    this.dropDown = this.dropDown.bind(this);
+    this.state = {
+      viewLinks: styles.hidden
+    }
+  }
+
+  dropDown(e) {
+    e.preventDefault();
+    let viewLinks = (this.state.viewLinks === styles.hidden) ? styles.viewable : styles.hidden;
+    this.setState({viewLinks: viewLinks})
+  }
+
   render() {
     return (
       <section className={styles.container}>
         <h2><Link to='/'>Book</Link></h2>
         <ul>
           <li><Link to="/freecontent">Free Content</Link></li>
-          <li>Mixing</li>
+          <div className={this.state.viewLinks}>
+            <Link to='/'>Mixing 1.1</Link>
+          </div>
+          <div className={this.state.viewLinks}>
+            <Link to='/'>Mixing 1.2</Link>
+          </div>
+          <li onClick={e => this.dropDown(e)}>Mixing</li>
         </ul>
       </section>
     )

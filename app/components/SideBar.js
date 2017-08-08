faf5b9588ee87d6fa921277fe775db0fabf13ccc
Thomas <thomasmaher1210@gmail.com>
2017-08-08 13:33:23 -0400

    Smoother transition for 'Mixing' dropdown in sidebar

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -7,13 +7,13 @@ export default class SideBar extends Component {
     super(props)
     this.dropDown = this.dropDown.bind(this);
     this.state = {
-      viewLinks: styles.hidden
+      viewLinks: styles.drop
     }
   }
 
   dropDown(e) {
     e.preventDefault();
-    let viewLinks = (this.state.viewLinks === styles.hidden) ? styles.viewable : styles.hidden;
+    let viewLinks = (this.state.viewLinks === styles.up) ? styles.drop : styles.up;
     this.setState({viewLinks: viewLinks})
   }
 
@@ -23,13 +23,17 @@ export default class SideBar extends Component {
         <h2><Link to='/'>Book</Link></h2>
         <ul>
           <li><Link to="/freecontent">Free Content</Link></li>
-          <div className={this.state.viewLinks}>
-            <Link to='/mixing/1.1'>Mixing 1.1</Link>
+          <div className={styles.mixes}>
+            <div>
+              <Link to='/mixing/1.1'>Mixing 1.1</Link>
+            </div>
+            <div>
+              <Link to='/mixing/1.2'>Mixing 1.2</Link>
+            </div>
           </div>
-          <div className={this.state.viewLinks}>
-            <Link to='/mixing/1.2'>Mixing 1.2</Link>
-          </div>
-          <li onClick={e => this.dropDown(e)}>Mixing</li>
+          <li className={this.state.viewLinks}
+            onClick={e => this.dropDown(e)}>Mixing</li>
+          <li> </li>
         </ul>
       </section>
     )

52b3d5fe24353ef8fa304f9f5ef856041d5efbf4
Thomas <thomasmaher1210@gmail.com>
2017-09-26 12:13:10 -0400

    Includes react-quill for user input

--- app/components/input/Create.jsx
+++ app/components/input/Create.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactQuill from 'react-quill';
 import styles from './Create.css';
 
 export default class Create extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      value: "",
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({value})
   }
 
   render() {
@@ -20,14 +26,19 @@ export default class Create extends Component {
         </div>
 
         <div className={styles.inputSection}>
-          <form>
-            <textarea rows="30" cols="115"
-              className={styles.inputField}></textarea>
-            <br />
-            <input type="submit" value="Save Content"></input>
-          </form>
+          <ReactQuill value={this.state.value}
+            onChange={this.handleChange}
+            className={styles.quillContainer}/>
         </div>
       </section>
     )
   }
 }
+
+
+  // <form>
+  //   <textarea rows="30" cols="115"
+  //     className={styles.inputField}></textarea>
+  //   <br />
+  //   <input type="submit" value="Save Content"></input>
+  // </form>
